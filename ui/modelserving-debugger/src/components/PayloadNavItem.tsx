64f3d51648917ae63c102119d3cd86e1f9ca3eb7
Rui Vieira <ruidevieira@googlemail.com>
2024-02-19 20:46:05 +0000

    Change nav item layout.
    Update README.md

--- ui/modelserving-debugger/src/components/PayloadNavItem.tsx
+++ ui/modelserving-debugger/src/components/PayloadNavItem.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { NavItem, Text, TextListItem, TextList } from '@patternfly/react-core';
+import {NavItem, Text, TextListItem, TextList, TextVariants} from '@patternfly/react-core';
 import { UploadIcon, DownloadIcon } from '@patternfly/react-icons';
 import { Timestamp, TimestampTooltipVariant } from '@patternfly/react-core';
 import { PayloadWithId } from './Payloads'; // Adjust the path as needed
@@ -11,20 +11,19 @@ const PayloadNavItem = ({ modelId, payload }: { modelId: string|undefined; paylo
             <NavLink
                 to={`/models/${modelId}/payloads/${payload.id}?kind=${payload.payload.kind}`}
             >
-                <TextList component="dl">
-                    <TextListItem component="dt">ID:</TextListItem>
-                    <TextListItem component="dd">{payload.id}</TextListItem>
-                    <TextListItem component="dt">Timestamp:</TextListItem>
-                    <TextListItem component="dd">
+                <TextList>
+                    <TextListItem>{payload.id}</TextListItem>
+                    <TextListItem>
                         <Timestamp
                             date={new Date(payload.Timestamp)}
                             tooltip={{ variant: TimestampTooltipVariant.default, suffix: 'Coordinated Universal Time (UTC)' }}
                         />
                     </TextListItem>
-                    <TextListItem component="dt">Kind:</TextListItem>
-                    <TextListItem component="dd">
+                    <TextListItem>
+                        <Text component={TextVariants.small}>
                         {payload.payload.kind === 'request' ? <UploadIcon /> : <DownloadIcon />}
                         [{payload.payload.kind}]
+                        </Text>
                     </TextListItem>
                 </TextList>
             </NavLink>
